953b99271406f567ea42b7780ba9dc81eb5f9ae5
Jeff Pang <jeff.tspang@gmail.com>
2022-04-02 14:54:30 +0800

    new routes

--- src/server/server_http_routes.go
+++ src/server/server_http_routes.go
@@ -7,8 +7,30 @@ import (
 )
 
 func initRoutes(r *gin.Engine, svr Server) {
+	r.GET("/options/template//k/*path", func(ctx *gin.Context) { getOptions(ctx, svr) })
+	r.PUT("/options/template/k/*path", func(ctx *gin.Context) { putOptions(ctx, svr) })
 	r.PUT("/template/:store/k/*path", func(ctx *gin.Context) { putTemplate(ctx, svr) })
 	r.GET("/template/:store/k/*path", func(ctx *gin.Context) { getTemplate(ctx, svr) })
+}
+
+func getOptions(ctx *gin.Context, svr Server) {
+
+	templatePath := ctx.Param("path")
+	result := svr.GetTemplateStoreOptions(templatePath)
+	ctx.String(http.StatusOK, result)
+}
+
+func putOptions(ctx *gin.Context, svr Server) {
+
+	templatePath := ctx.Param("path")
+	options, err := ctx.GetRawData()
+
+	if err != nil {
+		fmt.Errorf("Error putting template option %s", options)
+	}
+
+	result := svr.PutTemplateStoreOptions(templatePath, string(options))
+	ctx.String(http.StatusOK, result)
 }
 
 func putTemplate(ctx *gin.Context, svr Server) {
